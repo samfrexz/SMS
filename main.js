0164988b632cb77e119819087e4b6b2175172520
CodaarX <michaelisesele@gmail.com>
2020-11-21 13:51:32 +0100

    first commit. all parameters and methods have been outlined for uniformity

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("Hello from JavaScript");
+//  creating school object
+let School = {
+    name: " King's College",
+    address: " Lagos ",
+    student: [],
+    staff: [],
+    registerStudent: function(name, age, gender, sclass, schoolfee){},
+    getAllStudents: function(){},
+    getStudent: function(id){},
+    modifyStudent: function(id){},
+    deleteStudent: function(id){},
+    employStaff: function(name, gender, subject, yearsofexperience, qualification, contact){},
+    getAllStaffs: function(){},
+    getStaffId: function(id){},
+    modifyStaff: function(id){},
+    deleteStaff: function(id){},
+    getSchoolAccount: function(){},
+    getSchoolInfo: function(){},
+}
